dc66c473abe8a9335d80b75205d3158e99c51635
Alexis Mtz <alex.mtz873@gmail.com>
2021-02-27 22:37:50 -0600

    [Add](VistaPrincipal)

---
+++ proyecto-ia/src/componentes/vistaPrincipal/VistaPrincipal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styles from '../vistaPrincipal/vistaPrincipal.module.css';
+
+const VistaPrincipal = () => {
+  return (
+    <div>
+      hola prros
+    </div>
+  )
+}
+
+export default VistaPrincipal
